823b31a95b51ea1db4989cca986265ae89352156
heyimsourav <93369752+heyimsourav@users.noreply.github.com>
2021-10-29 00:47:57 -0700

    Delete models.py

--- models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class namedetail(models.Model):
-    std_name=models.CharField(max_length=20)
-    std_roll=models.IntegerField()
-    std_sec=models.CharField(max_length=1)
